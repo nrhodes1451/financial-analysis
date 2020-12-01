946683ad5a37d639b089469c42f422a3a074afe7
Nick <nick@nick-rhodes.co.uk>
2020-12-01 22:00:56 +0000

    began to work on fundamentals

--- proc/lse/join.R
+++ proc/lse/join.R
@@ -4,7 +4,7 @@ library(arrow)
 # load categorical data
 df_index <- read_csv("data/proc/lse/index.csv")
 df_listings <- read_csv("data/proc/lse/listings.csv")
-df_df_lookups <- read_csv("data/proc/lse/lookups.csv") %>% 
+df_lookups <- read_csv("data/proc/lse/lookups.csv") %>% 
   rename(code = `listings code`,
          `Company Name` = `index name`) %>% 
   select(-`lookup name`)
